903e72e2144b830473a4db6392dfee05b9279611
ChasePoin <mcstarcraft04@gmail.com>
2025-11-14 21:00:49 -0600

    ball

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -8,7 +8,32 @@ public class Ball : MonoBehaviour
     public float DirectionStrength;
     public float LargeSize = 5.00f;
     public float MediumSize = 2.50f;
-    public float SmallSize = 1.00f;
 
-    
+    public void AdjustSpeed(float delta)
+    {
+        Speed *= delta;
+    }
+    public void AdjustGravity(float delta)
+    {
+        GravityStrength *= delta;
+    }
+    public void AdjustDirection(float delta)
+    {
+        DirectionStrength *=  delta;
+    }
+    public string CheckSize(float size)
+    {
+        if (size >= LargeSize)
+        {
+            return "Large";
+        }
+        else if (size >= MediumSize && size <= LargeSize)
+        {
+            return "Medium";
+        }
+        else
+        {
+            return "Small";
+        }
+    }
 }
