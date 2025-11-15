029c067393b240a37646b7303acc4db21f2c1b18
ChasePoin <mcstarcraft04@gmail.com>
2025-11-15 12:38:02 -0600

    eff

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -183,23 +183,5 @@ public class PlayerController : MonoBehaviour
         Vector3 finalMove = (move * playerSpeed) + (playerVelocity.y * Vector3.up);
         controller.Move(finalMove * Time.deltaTime);
     }
-
-    // private void ApplyTransformations(Rigidbody ballRigid, Ball thisBall, InputAction.CallbackContext context)
-    // {
-    //     // // apply gravity effect
-    //     // ballRigid.mass *= thisBall.GravityStrength;
-    //     // // ball throw based off of speed
-    //     // Vector3 throwDir = playerCamera.transform.forward + playerCamera.transform.up * 0.2f;
-
-    //     // // on release
-    //     // if (context.canceled)
-    //     // {
-    //     //     ballRigid.AddForce(throwDir.normalized * thisBall.Speed * chargePower, ForceMode.Impulse);
-    //     //     // direction
-    //     //     Vector3 curve = new Vector3(thisBall.DirectionStrength, 0f, 0f);
-    //     //     ballRigid.AddForce(curve * Time.deltaTime, ForceMode.Force);
-    //     //     startCharge = false;
-    //     //     chargePower = 0;
-    //     }
 }
 
